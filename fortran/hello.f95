dbaaf78d3e1fd477f39c339c02ae441c858da668
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-02-05 22:41:29 -0500

    working on some function things.

--- fortran/hello.f95
+++ fortran/hello.f95
@@ -1,2 +1,14 @@
-PRINT *, "Hello World!"
-       END
+ FUNCTION func_name()
+    CHARACTER (LEN=*) :: func_name
+    func_name = "butt"
+    RETURN
+ END FUNCTION
+
+program hello
+CHARACTER (LEN=*), PARAMETER :: message =  "A STRING FOR ALAN"
+CHARACTER (LEN=*), PARAMETER :: test =  "bug"
+CHARACTER (LEN=*) :: func_name
+PRINT *, func_name()
+
+end program hello
+	   
