ef63e4ec5bc1e78b4389c7709b6b3c0daed5988f
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-02-08 22:09:27 -0500

    i think its stable and working for fortran?

--- fortran/hello.f95
+++ fortran/hello.f95
@@ -1,27 +1,33 @@
 subroutine encrypt(shift, message)
 	INTEGER :: shift
 	CHARACTER (*), intent(inout) :: message
-	INTEGER :: length
 	CHARACTER :: tempC
 	CHARACTER :: tempStr
-	INTEGER :: tempNum
+	INTEGER :: tempNum1
+	INTEGER :: tempNum2
 	CHARACTER (Len =len_Trim(message)) :: encryption
-	!CHARACTER :: encryption
-	length = len_Trim(message)
-	print *, encryption
-	
-	
-	
 	! 32 = blank, char converts from int to char
-	
-	do i=0, length
+	do i=0, len_Trim(message)
 			tempC = message(i:i+1) 
-			tempNum = IACHAR(tempC)
-			if (tempNum /= 32) then 
-			tempNum = tempNum + shift
+			tempNum1 = IACHAR(tempC)
+			!print *, tempC
+			!print *, tempNum1
+			
+			if (tempNum1 >= 65 .AND. tempNum1<=90 .AND. tempNum1 +shift <=90) then 
+				tempNum2 = tempNum1 +shift 
+				! need -26 if over 90
+			else if (tempNum1 >= 65 .AND. tempNum1<=90 .AND. tempNum1 +shift >90) then
+				tempNum2 = tempNum1 +shift -26
+			else if (tempNum1 >=97 .AND. tempNum1<=122 .AND. tempNum1 +shift <= 122) then
+				tempNum2 = tempNum1 +shift
+			else if (tempNum1 >=97 .AND. tempNum1<=122 .AND. tempNum1 +shift >122) then 
+				tempNum2 = tempNum1 +shift -26
+			else 
+				tempNum2 = 32
 			end if
-			tempC = CHAR(tempNum)
-			print *, tempC
+			
+			tempC = CHAR(tempNum2)
+			!print *, tempC
 			encryption(i:i+1) = tempC
 			
 	end do
@@ -30,11 +36,51 @@ subroutine encrypt(shift, message)
 	PRINT *, encryption
 end subroutine encrypt
 
+subroutine decrypt (shift, message)
+INTEGER :: shift
+	CHARACTER (*), intent(inout) :: message
+	CHARACTER :: tempC
+	CHARACTER :: tempStr
+	INTEGER :: tempNum1
+	INTEGER :: tempNum2
+	CHARACTER (Len =len_Trim(message)) :: decryption
+
+do i=0, len_Trim(message)
+			tempC = message(i:i+1) 
+			tempNum1 = IACHAR(tempC)
+			!print *, tempC
+			!print *, tempNum1
+			
+			if (tempNum1 >= 65 .AND. tempNum1<=90 .AND. tempNum1 +shift <=90) then 
+				tempNum2 = tempNum1 -shift 
+				! need -26 if over 90
+			else if (tempNum1 >= 65 .AND. tempNum1<=90 .AND. tempNum1 +shift >90) then
+				tempNum2 = tempNum1 -shift +26
+			else if (tempNum1 >=97 .AND. tempNum1<=122 .AND. tempNum1 +shift <= 122) then
+				tempNum2 = tempNum1 -shift
+			else if (tempNum1 >=97 .AND. tempNum1<=122 .AND. tempNum1 +shift >122) then 
+				tempNum2 = tempNum1 -shift +26
+			else 
+				tempNum2 = 32
+			end if
+			
+			tempC = CHAR(tempNum2)
+			!print *, tempC
+			decryption(i:i+1) = tempC
+			
+	end do
+	
+	PRINT *, message
+	PRINT *, decryption
+end subroutine decrypt 
 program hello
 
 CHARACTER (LEN=*), PARAMETER:: test =  "A STRING FOR ALAN"
+CHARACTER (LEN=*), PARAMETER:: test2 =  "a bUtt"
 
-call encrypt(1, test)
+!call encrypt(1, test)
+call encrypt(27, test2)
+call decrypt(27, "b cVuu")
 
 end program hello
 	   
