762e04740bd18afa1031f1860a43d0ca6b21a85a
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-02-08 21:14:01 -0500

    figured out strings?

--- fortran/hello.f95
+++ fortran/hello.f95
@@ -1,14 +1,40 @@
- FUNCTION func_name()
-    CHARACTER (LEN=*) :: func_name
-    func_name = "butt"
-    RETURN
- END FUNCTION
+subroutine encrypt(shift, message)
+	INTEGER :: shift
+	CHARACTER (*), intent(inout) :: message
+	INTEGER :: length
+	CHARACTER :: tempC
+	CHARACTER :: tempStr
+	INTEGER :: tempNum
+	CHARACTER (Len =len_Trim(message)) :: encryption
+	!CHARACTER :: encryption
+	length = len_Trim(message)
+	print *, encryption
+	
+	
+	
+	! 32 = blank, char converts from int to char
+	
+	do i=0, length
+			tempC = message(i:i+1) 
+			tempNum = IACHAR(tempC)
+			if (tempNum /= 32) then 
+			tempNum = tempNum + shift
+			end if
+			tempC = CHAR(tempNum)
+			print *, tempC
+			encryption(i:i+1) = tempC
+			
+	end do
+	
+	PRINT *, message
+	PRINT *, encryption
+end subroutine encrypt
 
 program hello
-CHARACTER (LEN=*), PARAMETER :: message =  "A STRING FOR ALAN"
-CHARACTER (LEN=*), PARAMETER :: test =  "bug"
-CHARACTER (LEN=*) :: func_name
-PRINT *, func_name()
+
+CHARACTER (LEN=*), PARAMETER:: test =  "A STRING FOR ALAN"
+
+call encrypt(1, test)
 
 end program hello
 	   
