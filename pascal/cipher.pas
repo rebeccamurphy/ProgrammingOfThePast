36c0046a2188f127000391f533fc68a4ae87aaa2
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-02-23 22:54:25 -0500

    i think pascal is all good.

--- pascal/cipher.pas
+++ pascal/cipher.pas
@@ -1,9 +1,8 @@
 program cipher;
 var 
     test :string;
-    
 function encrypt(message :string; shift: integer): string;
-		var (* local variable declaration *)
+    	var (* local variable declaration *)
 			encryption :string;
 			tempShift : integer;
 			charNum :integer;
@@ -32,6 +31,62 @@ function encrypt(message :string; shift: integer): string;
 				end;
 			encrypt := encryption;
 		end;
+
+function decrypt(message :string; shift: integer): string;
+		var (* local variable declaration *)
+			decryption :string;
+			tempShift : integer;
+			charNum :integer;
+			i: integer;
+		begin 
+            decryption := '';
+			if (shift > 26) then
+				tempShift := shift mod 26
+			else 
+				tempShift := shift;
+				
+			for i:=1 to length(message) do
+				begin
+					charNum := Ord(message[i]);
+					if (charNum >= 65) and (charNum<=90) and (charNum - tempShift >=65) then 
+						charNum := charNum -tempShift
+					else if (charNum >= 65) and (charNum<=90) and (charNum -tempShift <65) then
+						charNum := charNum - tempShift +26
+					else if (charNum >=97) and (charNum<=122) and (charNum -tempShift >= 97) then
+						charNum := charNum -tempShift
+					else if (charNum >=97) and (charNum<=122) and (charNum -tempShift <97) then 
+						charNum := charNum - tempShift +26
+					else 
+						charNum := 32;
+				decryption:= decryption+ Chr(charNum);
+				end;
+			decrypt := decryption;
+		end;
+procedure solve(message:string; maxShiftValue:integer);
+	var
+		i:integer;
 	begin
-	writeln(encrypt('ABC', 1))
+	for i := maxShiftValue downto 0 do
+		begin
+			writeln('Caesar ', i, ': ', encrypt(message, i));
+		end;
+	end;
+	begin
+	test:= 'A STRING FOR ALAN';
+	writeln('..BEEP BOOP ENCRYPTING...');
+	writeln();
+	writeln('ORIGINAL: ', test);
+	writeln('SECRET: ', encrypt(test, 100));
+	writeln();
+	
+	writeln('..BOOP BEEP DECRYPTING...');
+	writeln();
+	
+	writeln('ORIGINAL: ', 'XQPPO');
+	writeln('SECRET: ', decrypt('XQPPO', 100));
+	writeln();
+	
+	writeln('...BEEP SOLVING BOOP...');
+	solve('hal', 26);
+	
 	end.
