f1da72c72e5b7cc8c553b2780f4b81b8c161be71
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-03-31 12:37:41 -0400

    got encrypt working

--- lisp/cipher.lisp
+++ lisp/cipher.lisp
@@ -1,29 +1,33 @@
-(defun triple (X)
-  "Compute three times X."  ; Inline comments can
-  (* 3 X))                  ; be placed here. nil is a thing car gets first element, cdr gets the rest
+; Inline comments can
+;be placed here. nil is a thing car gets first element, cdr gets the rest
 ;comment? append adds lists together 
 ;run with (load 'cipher.lisp") (triple 3)
 ; setq is used to set variables also you can use '(1 2 3) instead of (list 1 2 3)
-(defun factorial (N)
-  "Compute the factorial of N."
-  (if (= N 1)
-      1
-    (* N (factorial (- N 1)))))
-(defun test(chr shift)
+"(defvar fares '(31.14 28.12 25.10 22.08 19.06 16.04 13.02 10))
+
+(defun plus-extra (fare) (+ 3.02 fare))
+(map 'list 'plus-extra fares)
+"
+(defun test(chr)
 	(let( (num (char-code chr))
 		  (shifted (+ (char-code chr) shift))
 		)
 	(cond 
 		((= num 32)
-			3)
+			(code-char 32))
 		;(( M + Shift > 90 andalso M< 90) orelse ( M +Shift > 122))
-		((or (and (< shifted 90) (> num 90)) (< shifted 122)) 	
-			2)
+		((or (and (> shifted 90) (< num 90)) (> shifted 122)) 	
+			(code-char (+ shifted -26)))
 		;(( M + Shift < 65 andalso M<90) orelse(M+Shift<97 andalso M>=97)) 
-		((or (and (> shifted 65) (> num 90)) (and (> shifted 97) (< num 96))) 	
-			1)
-		(t 0)
+		((or (and (< shifted 65) (< num 90)) (and (< shifted 97) (> num 96))) 	
+			1);(code-char(+ shifted 26)))
+		(t  (code-char shifted))
 		
 	)
 	)
 )
+(defun encrypt(message shft)
+	(defvar shift shft)
+	(setf shift shft)
+	(map 'string 'test message)
+	)
